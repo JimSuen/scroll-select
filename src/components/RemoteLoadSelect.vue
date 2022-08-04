07267ee78e51d78fc854ef7f440b428d4a6d010e
JimSuen <im.jimsuen@outlook.com>
2022-08-04 15:13:06 +0800

    feat: 实现模糊匹配

--- src/components/RemoteLoadSelect.vue
+++ src/components/RemoteLoadSelect.vue
@@ -3,7 +3,6 @@
     ref="selector"
     v-bind="$props"
     v-model="selectedValue"
-    filterable
     :loading="loading"
     :filter-method="handleFilter"
     @change="handleChange"
@@ -14,9 +13,9 @@
       :value="typeof item === 'object' ? item.value : item"
       :label="typeof item === 'object' ? item.label : item"
     ></el-option>
-    <el-option disabled v-show="!isFinish" value="-" key="-">
+    <span v-show="!isFinish && displayOptions.length" class="loading__text">
       数据加载中...
-    </el-option>
+    </span>
   </el-select>
 </template>
 <script setup lang="ts">
@@ -34,30 +33,32 @@ interface Props {
   modelValue?: string | number | number[] | string[];
   total: number;
   // eslint-disable-next-line no-unused-vars
-  optionFormatter: (value: string | number) => ObjectOption | string | number; // 将选中值转为下拉框的格式转换
+  optionFormatter?: (value: string | number) => ObjectOption | string | number; // 将选中值转为下拉框的格式转换
 }
 
 const props = withDefaults(defineProps<Props>(), {
   total: 0,
   optionFormatter: (value: string | number) => value,
 });
-const emits = defineEmits(["load", "update:modelValue"]);
+const emits = defineEmits(["load", "update:modelValue", "change"]);
 
 // 绑定值更新
 const selectedValue = ref(props.modelValue);
 const handleChange = (value: string | number) => {
   emits("update:modelValue", value);
+  emits("change", value);
 };
 
 const { total } = toRefs(props);
-const index = ref(0);
+const index = ref(1);
 const displayOptions = ref<Options<ObjectOption | string | number>>([]);
+const filterKey = ref<string | undefined>();
 const loading = ref(true);
 const selector = ref<any | null>(null);
 let scrollPosition = 0; // 记录当前滚动位置， 用来区分是下拉还是上拉
 
 const isFinish = computed(() => {
-  return total.value === displayOptions.value.length;
+  return displayOptions.value.length >= total.value;
 });
 
 const initSelectedOptions = () => {
@@ -77,13 +78,24 @@ const initSelectedOptions = () => {
   // }
 };
 
-const loadNewOptions = async (idx: number, filterKey?: string) => {
+const loadNewOptions = async (idx: number) => {
   loading.value = true;
   await emits(
     "load",
-    { index: idx, filterKey },
+    { index: idx, filterKey: filterKey.value },
     (list: Options<ObjectOption | string | number>) => {
-      displayOptions.value.push(...list);
+      const filteredOptions = list.filter((item) => {
+        if (typeof item === "object") {
+          return !(displayOptions.value as Options<ObjectOption>).find(
+            (displayItem) =>
+              displayItem.value === item.value &&
+              displayItem.key === item.key &&
+              displayItem.label === item.label
+          );
+        }
+        return !displayOptions.value.includes(item);
+      });
+      displayOptions.value.push(...filteredOptions);
     }
   );
   loading.value = false;
@@ -96,15 +108,24 @@ const pushSelectedOptions = () => {
   }
 };
 
+let allowFilter = true;
 const handleFilter = (val: string) => {
-  scrollPosition = 0; // 下拉框位置重置
-  index.value = 0;
-  displayOptions.value.length = 0;
-  pushSelectedOptions();
-  loadNewOptions(index.value, val);
+  filterKey.value = val;
+  if (!allowFilter) return;
+  allowFilter = false;
+  setTimeout(() => {
+    scrollPosition = 0; // 下拉框位置重置
+    index.value = 1;
+    displayOptions.value.length = 0;
+    pushSelectedOptions();
+    loadNewOptions(index.value);
+    allowFilter = true;
+  }, 500);
 };
+
 let allowLoad = true;
 const handleOptionsScroller = (event: Event) => {
+  if (isFinish.value) return;
   if (allowLoad) {
     allowLoad = false;
     setTimeout(() => {
@@ -152,6 +173,24 @@ const init = () => {
 };
 
 onMounted(init);
+
+defineExpose({
+  reload: () => {
+    displayOptions.value.length = 0;
+    index.value = 1;
+    filterKey.value = undefined;
+    loadNewOptions(index.value);
+  },
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+.loading__text {
+  display: block;
+  height: 36px;
+  line-height: 36px;
+  font-size: 14px;
+  color: #c0c4cc;
+  text-align: center;
+}
+</style>
